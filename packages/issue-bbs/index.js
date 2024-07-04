d030b9a287eeab857fb178ac5e196d6a53e8d688
Matthew Torres <matthew.torres211@gmail.com>
2024-07-04 22:36:00 +0800

    feature: add logs

--- packages/issue-bbs/index.js
+++ packages/issue-bbs/index.js
@@ -1,7 +1,10 @@
-import { add } from '@mono/core'
+import { add } from '@mateeyow/core'
 
 export function issueBBS() {
   console.log('issue-bbs');
   console.log('do something else')
   return add(1, 2);
 }
+
+const solution = issueBBS()
+console.log('solution', solution);
--- packages/issue-ecdsa/index.js
+++ packages/issue-ecdsa/index.js
@@ -1,7 +1,10 @@
-import { add } from '@mono/core'
+import { add } from '@mateeyow/core'
 
 export function issueECDSA() {
   console.log('issue-ECDSA');
   console.log('do something else')
   return add(1, 2);
 }
+
+const solution = issueECDSA()
+console.log('solution', solution);
